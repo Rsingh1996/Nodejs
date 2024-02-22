0610bd58cb389e59ae3647ce8a9ce5781a906e2d
Rahul <rahulna1996@gmail.com>
2024-02-22 17:22:06 +0530

    Iteration over a nested object

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+// Sum of N Number
+
 function sunOfNNumbers(...arg) {
   return arg.reduce((val, acc) => (acc = acc + val));
 }
 const result = sunOfNNumbers(2, 3, 4, 5);
 console.log(result);
 
+// Reverse of a String
 function reverseOfString(str) {
   let string = "";
   let stringLength = str.length;
@@ -18,6 +21,7 @@ function reverseOfString(str) {
 }
 console.log(reverseOfString("Rahul Singh"));
 
+// Question on Shallow copy
 const array1 = [
   {
     name: "candidate1",
@@ -35,3 +39,31 @@ const array3 = [...array1];
 array2[0].name = "Rahul";
 console.log(array3[0].name);
 //	what is the value of array3[0].name?
+
+// Iteration over a nested object
+
+const nestedObject = {
+  key1: "value1",
+  key2: {
+    key3: "value3",
+    key4: "value4",
+    key5: {
+      key6: "value6",
+      key7: {
+        key8: "value8",
+      },
+    },
+  },
+};
+
+const getKey = (obj) => {
+  let keys = [];
+  for (let key in obj) {
+    keys.push(key);
+    if (typeof obj[key] === "object") {
+      keys = keys.concat(getKey(obj[key]));
+    }
+  }
+  return keys;
+};
+console.log(getKey(nestedObject));
