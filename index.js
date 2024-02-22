cc87b3eff4ddb49c22b190270d01f528add1a7f0
Rahul <rahulna1996@gmail.com>
2024-02-22 21:30:22 +0530

    modified  index.js

--- index.js
+++ index.js
@@ -4,22 +4,17 @@ function sunOfNNumbers(...arg) {
   return arg.reduce((val, acc) => (acc = acc + val));
 }
 const result = sunOfNNumbers(2, 3, 4, 5);
-console.log(result);
+console.log("Sum of N numbers: " + result);
 
 // Reverse of a String
 function reverseOfString(str) {
   let string = "";
-  let stringLength = str.length;
-
-  //   for (let i = 0; i < stringLength; i++) {
-  //     string = str[i] + string;
-  //   }
-  for (let i = stringLength - 1; i >= 0; i--) {
-    string += str[i];
+  for (let i in str) {
+    string = str[i] + string;
   }
   return string;
 }
-console.log(reverseOfString("Rahul Singh"));
+console.log("ReverseString: " + reverseOfString("Rahul"));
 
 // Question on Shallow copy
 const array1 = [
@@ -37,7 +32,7 @@ const array1 = [
 const array2 = array1;
 const array3 = [...array1];
 array2[0].name = "Rahul";
-console.log(array3[0].name);
+console.log("array3[0].name: " + array3[0].name);
 //	what is the value of array3[0].name?
 
 // Iteration over a nested object
@@ -66,4 +61,26 @@ const getKey = (obj) => {
   }
   return keys;
 };
-console.log(getKey(nestedObject));
+console.log("All keys in object: " + getKey(nestedObject));
+
+//Find the Longest Word in a String
+
+function findLongestWordLength(str) {
+  const strArray = str.split(" ");
+
+  // by using map function and spread operator
+  const length = strArray.map((text) => text.length);
+  const longestWordLength = Math.max(...length);
+
+  // by using for loop
+  let maxLength = 0;
+  for (let word of strArray) {
+    let workLength = word.length;
+    if (workLength > maxLength) {
+      maxLength = workLength;
+    }
+  }
+  console.log(longestWordLength);
+  console.log(maxLength);
+}
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
