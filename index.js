d058e2b2cc92cc8ac00554c840f354c415517a67
Rahul <rahulna1996@gmail.com>
2024-02-25 01:21:08 +0530

    Truncate a String

--- index.js
+++ index.js
@@ -147,5 +147,21 @@ function repeatStringNumTimes(str, num) {
   }
   return newStr;
 }
-
 console.log(repeatStringNumTimes("abc", 3));
+
+/* 
+Truncate a String
+Truncate a string (first argument) if it is longer than the given maximum string length (second argument). 
+Return the truncated string with a ... ending
+*/
+function truncateString(str, num) {
+  let length = str.length;
+  let newStr = "";
+  if (num < length) {
+    newStr = str.substring(0, num) + "...";
+  } else newStr = str.substring(0, num);
+  console.log(newStr);
+  return newStr;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
