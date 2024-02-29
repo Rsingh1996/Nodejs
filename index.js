12fa262f2458046355a2b6bb4afb25417a26d67c
Rahul <rahulna1996@gmail.com>
2024-02-29 21:45:37 +0530

    Diff of Two Arrays

--- index.js
+++ index.js
@@ -266,3 +266,22 @@ function sumAll(arr) {
 }
 
 console.log("sum :", sumAll([1, 4]));
+/*
+Diff Two Arrays
+Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. 
+In other words, return the symmetric difference of the two arrays.
+*/
+
+function diffArray(arr1, arr2) {
+  let newArr = [];
+  newArr = arr1.concat(arr2);
+
+  let diff = newArr.filter((num) => {
+    if (arr1.indexOf(num) === -1 || arr2.indexOf(num) === -1) {
+      return num;
+    }
+  });
+  console.log(diff);
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 7, 3, 4, 5]);
