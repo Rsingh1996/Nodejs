b2f9d298f4cada37d8c465eebc16d2d0aed11874
Rahul <rahulna1996@gmail.com>
2024-03-01 19:41:17 +0530

    Seek and Destroy

--- index.js
+++ index.js
@@ -295,3 +295,28 @@ function removeDulicate(arr) {
 }
 
 removeDulicate([1, 2, 2, 3, 4, 5, 2, 5]);
+
+/*
+Seek and Destroy
+You will be provided with an initial array as the first argument to the destroyer function, followed by one or more arguments. 
+Remove all elements from the initial array that are of the same value as these arguments.
+
+The function must accept an indeterminate number of arguments, also known as a variadic function. 
+You can access the additional arguments by adding a rest parameter to the function definition or using the arguments object.
+
+*/
+
+function destroyer(arr, ...rest) {
+  let newArr = [];
+  newArr = arr.concat(rest);
+  let array = newArr.filter((num) => {
+    if (arr.includes(num) == !rest.includes(num)) {
+      return num;
+    }
+  });
+  console.log(array);
+  return array;
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+destroyer(["tree", "hamburger", 53], "tree", 53);
