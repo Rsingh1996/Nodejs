78e6f1dd0c300ed4485af19cd2a1484e81c15a77
Rahul <rahulna1996@gmail.com>
2024-03-05 23:06:28 +0530

    Sum All Primes

--- index.js
+++ index.js
@@ -319,3 +319,31 @@ function destroyer(arr, ...rest) {
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
 destroyer(["tree", "hamburger", 53], "tree", 53);
+
+/*
+Sum All Primes
+A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. 
+For example, 2 is a prime number because it is only divisible by 1 and 2. 
+In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
+Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+*/
+
+function sumPrimes(num) {
+  let sum = 0;
+  for (let i = 2; i <= num; i++) {
+    let isPrime = true;
+    for (let j = 2; j <= Math.sqrt(i); j++) {
+      if (i !== j && i % j === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      sum += i;
+    }
+  }
+  console.log(sum);
+  return sum;
+}
+
+sumPrimes(10);
