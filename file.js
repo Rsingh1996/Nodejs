72eb799aa5b3dbd8530ac076f2415c9d33f18cf3
Rahul <rahulna1996@gmail.com>
2024-03-11 10:38:51 +0530

    global and non-global modules

--- file.js
+++ file.js
@@ -1,3 +1,9 @@
 let file = require("./app");
 
+// console is a global module as we have not imported this, it's in-built
 console.log(file.foo());
+
+// the module which we have to import are called non global module
+
+const fs = require("fs");
+fs.writeFileSync("hello.txt", "Hello Rahul Singh");
