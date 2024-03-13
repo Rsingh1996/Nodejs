c7f80cc820d228c5df32e67aa8fe1e0d91e932c5
Rahul <rahulna1996@gmail.com>
2024-03-13 23:01:14 +0530

    created a simple API

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require("http"); // In nodejs, it handles the request and response of server
+const userData = require("./userData");
 http
   .createServer((req, resp) => {
-    resp.write("<h1>Hello Server</h1>");
+    resp.writeHead(200, { "Content-Type": "application/json" });
+    resp.write(JSON.stringify(userData));
     resp.end();
   })
   .listen(5000);
---
+++ userData.js
@@ -0,0 +1,9 @@
+const userData = {
+  username: "Rahul Singh",
+  email: "rahul@test.com",
+  contact: "+91-1234567890",
+  hobby: "Coding",
+  designation: "Software Developer",
+  Country: "India",
+};
+module.exports = userData;
