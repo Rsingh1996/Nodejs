ed48f91fda55a7639ae028889b50009a61ff3af5
Rahul <rahulna1996@gmail.com>
2024-03-13 23:03:39 +0530

    added more data to API

--- userData.js
+++ userData.js
@@ -1,9 +1,27 @@
-const userData = {
-  username: "Rahul Singh",
-  email: "rahul@test.com",
-  contact: "+91-1234567890",
-  hobby: "Coding",
-  designation: "Software Developer",
-  Country: "India",
-};
+const userData = [
+  {
+    username: "Rahul Singh",
+    email: "rahul@test.com",
+    contact: "+91-1234567890",
+    hobby: "Coding",
+    designation: "Software Developer",
+    Country: "India",
+  },
+  {
+    username: "Ankit Gupta",
+    email: "ankit@test.com",
+    contact: "+91-1234567890",
+    hobby: "Coding",
+    designation: "Software Developer",
+    Country: "India",
+  },
+  {
+    username: "Chandan Kumar",
+    email: "chandu@test.com",
+    contact: "+91-1234567890",
+    hobby: "Coding",
+    designation: "Software Developer",
+    Country: "India",
+  },
+];
 module.exports = userData;
