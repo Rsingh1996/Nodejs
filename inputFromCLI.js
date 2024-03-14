51adaddee949b3339c0c15a5185f0c900b1fdb54
Rahul <rahulna1996@gmail.com>
2024-03-14 20:58:08 +0530

    muliple files creatation at a specific pathusing Path module and Loop

--- inputFromCLI.js
+++ inputFromCLI.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 
+/*
 const input = process.argv;
 
 if (input[2] == "add") {
@@ -8,4 +9,15 @@ if (input[2] == "add") {
   fs.unlinkSync(input[3]);
 } else {
   console.log("Invalid Input");
+}
+*/
+const path = require("path");
+const dirPath = path.join(__dirname, "files");
+
+// to create multiple files
+for (let i = 1; i <= 5; i++) {
+  fs.writeFileSync(
+    `${dirPath}/file${i}.txt`,
+    "A Simple text files created by loop"
+  );
 }
