6f23b9abbff995b9bf94f782a09e9fe6ad9a0813
Rahul <rahulna1996@gmail.com>
2024-03-18 00:16:18 +0530

    Server creation

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const PORT = 4000;
 const app = express();
 
 app.get("", (request, response) => {
@@ -7,3 +8,9 @@ app.get("", (request, response) => {
 app.get("/about", (request, response) => {
   response.send("Hello from about page!");
 });
+app.get("/help", (request, response) => {
+  response.send("Hello from help page!");
+});
+///////////////// Server creation ////////////////////////////////
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
