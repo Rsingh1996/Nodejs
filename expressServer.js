4db07c36a6c76f7142c52768d65e36f314e18de6
Rahul <rahulna1996@gmail.com>
2024-03-25 00:27:27 +0530

    updated condition

--- expressServer.js
+++ expressServer.js
@@ -12,7 +12,7 @@ app.set("view engine", "ejs");
 const requestFilter = (req, resp, next) => {
   if (!req.query.age) {
     resp.send("<h2>Page not available</h2>");
-  } else if (req.query.age > 18) {
+  } else if (req.query.age < 18) {
     resp.send("<h2>Page not accessible for age below 18</h2>");
   } else {
     next();
