fb9de5ea8501028a66ddcd0ba14a03b312d4e077
Rahul <rahulna1996@gmail.com>
2024-03-28 23:10:44 +0530

    Post API created and tested successfully

--- api.js
+++ api.js
@@ -3,11 +3,19 @@ const dbConnection = require("./mongodb");
 
 const PORT = 5200;
 const app = express();
+app.use(express.json());
+
 app.get("/", async (req, resp) => {
   let data = await dbConnection();
   let result = await data.find().toArray();
   console.log(result);
   resp.send({ result });
+});
+
+app.post("/", async (req, resp) => {
+  let data = await dbConnection();
+  let result = await data.insertOne(req.body);
+  resp.send(req.body);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
