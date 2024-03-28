fafce6a7721fad390e392abd4f859f8fb625ed70
Rahul <rahulna1996@gmail.com>
2024-03-28 22:28:41 +0530

    updated condition to check if Data Requested for delete is available in DB or not

--- deleteData.js
+++ deleteData.js
@@ -3,10 +3,12 @@ const dbConnection = require("./mongodb");
 const deleteData = async () => {
   console.log("Delete Data function called!");
   let data = await dbConnection();
-  //   let result = await data.deleteOne({ name: "Nokia 1400" });
-  let result = await data.deleteMany({ name: "Nokia 1400" });
+  let result = await data.deleteOne({ name: "S24" });
+  //   let result = await data.deleteMany({ name: "Nokia 1400" });
 
-  if (result.acknowledged) {
+  if (result.deletedCount === 0) {
+    console.log("Data Requested for delete is not available in DB");
+  } else if (result.acknowledged) {
     console.log("Data Deleted Successfully!");
   }
   console.log(result);
