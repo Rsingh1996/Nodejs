bbb3c7e585a1c6832ab49e1aff432942f24e4f3b
Rahul <rahulna1996@gmail.com>
2024-03-28 22:24:44 +0530

    perfromed delete operation on DB using deleteOne and deleteMany function

--- deleteData.js
+++ deleteData.js
@@ -1,6 +1,14 @@
 const dbConnection = require("./mongodb");
 
-const deleteData = () => {
+const deleteData = async () => {
   console.log("Delete Data function called!");
+  let data = await dbConnection();
+  //   let result = await data.deleteOne({ name: "Nokia 1400" });
+  let result = await data.deleteMany({ name: "Nokia 1400" });
+
+  if (result.acknowledged) {
+    console.log("Data Deleted Successfully!");
+  }
+  console.log(result);
 };
 deleteData();
