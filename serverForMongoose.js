e26a32f44d6fb0a55ff432d80842b34fa1cd6fc8
Rahul <rahulna1996@gmail.com>
2024-03-30 23:02:20 +0530

    update operation performed on DB

--- serverForMongoose.js
+++ serverForMongoose.js
@@ -1,25 +1,35 @@
 const mongoose = require("mongoose");
 
-const main = async () => {
-  mongoose
-    .connect("mongodb://localhost:27017/e-comm")
-    .then(() => console.log("DB Connected!"));
+mongoose
+  .connect("mongodb://localhost:27017/e-comm")
+  .then(() => console.log("DB Connected!"));
 
-  const ProductSchema = new mongoose.Schema({
-    name: { type: String },
-    brand: { type: String },
-    price: { type: Number },
-    category: { type: String },
-  });
+const ProductSchema = new mongoose.Schema({
+  name: { type: String },
+  brand: { type: String },
+  price: { type: Number },
+  category: { type: String },
+});
 
+const addDataInDB = async () => {
   const ProductModel = new mongoose.model("products", ProductSchema);
   const instance = new ProductModel({
-    name: "OnePlus Nord",
-    price: 10000,
+    name: "OnePlus 7T",
+    price: 100000,
     brand: "OnePlus",
     category: "Mobile",
   });
   let result = await instance.save();
   console.log(result);
 };
-main();
+// addDataInDB();
+
+const updateDataInDB = async () => {
+  const ProductModel = new mongoose.model("products", ProductSchema);
+  const instance = await ProductModel.updateOne(
+    { name: "OnePlus 7T" },
+    { $set: { price: 90000, name: "OnePlus 8T" } }
+  );
+  console.log(instance);
+};
+updateDataInDB();
