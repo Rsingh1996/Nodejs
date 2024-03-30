4b45642e2a0a5c826a8cf683074efd67af14f625
Rahul <rahulna1996@gmail.com>
2024-03-30 22:39:15 +0530

    created product schema and model to insert item in DB

--- serverForMongoose.js
+++ serverForMongoose.js
@@ -1,1 +1,20 @@
 const mongoose = require("mongoose");
+
+const main = async () => {
+  mongoose
+    .connect("mongodb://localhost:27017/e-comm")
+    .then(() => console.log("DB Connected!"));
+
+  const ProductSchema = new mongoose.Schema({
+    name: { type: String },
+    // brand: { type: String },
+    // price: { type: Number },
+    // category: { type: String },
+  });
+
+  const ProductModel = new mongoose.model("products", ProductSchema);
+  const instance = new ProductModel({ name: "Nokia 26" });
+  let result = await instance.save();
+  console.log(result);
+};
+main();
