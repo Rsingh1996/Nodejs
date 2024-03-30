5c526bcc45c8e676143898867006e12e7fcff8d8
Rahul <rahulna1996@gmail.com>
2024-03-30 22:43:07 +0530

    updated Product Schema

--- serverForMongoose.js
+++ serverForMongoose.js
@@ -7,13 +7,18 @@ const main = async () => {
 
   const ProductSchema = new mongoose.Schema({
     name: { type: String },
-    // brand: { type: String },
-    // price: { type: Number },
-    // category: { type: String },
+    brand: { type: String },
+    price: { type: Number },
+    category: { type: String },
   });
 
   const ProductModel = new mongoose.model("products", ProductSchema);
-  const instance = new ProductModel({ name: "Nokia 26" });
+  const instance = new ProductModel({
+    name: "OnePlus Nord",
+    price: 10000,
+    brand: "OnePlus",
+    category: "Mobile",
+  });
   let result = await instance.save();
   console.log(result);
 };
