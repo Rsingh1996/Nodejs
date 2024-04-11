0b802c71f34d464812dad9c751f320127621f9e5
Rahul <rahulna1996@gmail.com>
2024-04-11 11:56:17 +0530

    Event in node js

--- eventEmitter.js
+++ eventEmitter.js
@@ -3,8 +3,24 @@ const EventEmitter = require("events");
 const PORT = 9570;
 const app = express();
 
+const event = new EventEmitter();
+let count = 0;
+event.on("countAPI", () => {
+  count++;
+  console.log("Event called", count);
+});
+
 app.get("/", (req, resp) => {
-  resp.send("API called!");
+  resp.send("Get API called!");
+  event.emit("countAPI");
+});
+app.get("/search", (req, resp) => {
+  resp.send("Search API called!");
+  event.emit("countAPI");
+});
+app.get("/update", (req, resp) => {
+  resp.send(" Update API called!");
+  event.emit("countAPI");
 });
 
 app.listen(PORT, () => console.log(`Server is listing ${PORT}`));
